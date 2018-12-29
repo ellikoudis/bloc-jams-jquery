18273b5c0173bdf60a08e8903a6d33d05ce6557e
Elena <ellikoudis@gmail.com>
2018-12-29 15:21:26 -0500

    fixed html

--- scripts/player.js
+++ scripts/player.js
@@ -13,36 +13,42 @@ class Player {
   getTime() {
     return this.soundObject.getTime();
   }
-  
+
   playPause (song = this.currentlyPlaying) {
+    console.log("get duration 1 = " + player.getDuration());
+
     if (this.currentlyPlaying !== song) {
       // Stop the currently playing sound file (even if nothing is playing)
       this.soundObject.stop();
       // Clear classes on the song that's currently playing
       this.currentlyPlaying.element.removeClass('playing paused');
-      
+
       // Update our currentlyPlaying and playState properties
       this.currentlyPlaying = song;
       this.playState = 'stopped';
       this.soundObject = new buzz.sound(this.currentlyPlaying.soundFileUrl);
+      console.log("get duration 2 = " + player.getDuration());
     }
     if (this.playState === 'paused' || this.playState === 'stopped') {
       this.soundObject.setVolume( this.volume );
       this.soundObject.play();
       this.playState = 'playing';
       this.currentlyPlaying.element.removeClass('paused').addClass('playing');
+      console.log("get duration 3 = " + player.getDuration());
     } else {
       this.soundObject.pause();
       this.playState = 'paused';
       this.currentlyPlaying.element.removeClass('playing').addClass('paused');
+      console.log("get duration 4 = " + player.getDuration());
     }
+    console.log("get duration 5 = " + player.getDuration());
   }
-  
+
   skipTo (percent) {
     if (this.playState !== 'playing') { return }
     this.soundObject.setTime( (percent / 100) * this.soundObject.getDuration() );
   }
-  
+
   setVolume (percent) {
     this.volume = percent;
     this.soundObject.setVolume(percent);
@@ -50,4 +56,3 @@ class Player {
 }
 
 const player = new Player();
-
