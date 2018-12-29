33f3e56a9176e8f92d15e924081325b38d094579
Elena <ellikoudis@gmail.com>
2018-12-29 11:56:08 -0500

    assignment work

---
+++ scripts/helper.js
@@ -0,0 +1,18 @@
+class Helper {
+
+  playPauseAndUpdate(song){
+
+    player.playPause(song);
+    let totalTime = null;
+    if(song !== undefined){
+      totalTime = song.duration;
+    } else {
+      totalTime = player.getDuration();
+    }
+
+    $('#time-control .total-time').text( totalTime );
+
+  };
+}
+
+const helper = new Helper();
